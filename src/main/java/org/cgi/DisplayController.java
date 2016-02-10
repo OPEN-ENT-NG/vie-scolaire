ed9ace3952468d722995d1af424a0d53fb259271
LEDUNOIS Simon <simon.Ledunois@cgi.com>
2016-02-10 14:09:11 +0100

    Modification Architecture

--- src/main/java/org/cgi/DisplayController.java
+++ src/main/java/org/cgi/DisplayController.java
@@ -23,10 +23,27 @@ public class DisplayController extends ControllerHelper {
             @Override
             public void handle(UserInfos user) {
                 if(user.getType().equals("Teacher")) {
-                    renderView(request, null, "viescolaire/viescolaire.html", null);
+                    renderView(request, null, "viescolaire/vsco_teacher.html", null);
+                }else if(user.getType().equals("Personnel")){
+                    renderView(request, null, "viescolaire/vsco_personnel.html", null);
                 }
 
                 // TODO rediriger sur le bon fichier pour les CPE
+            }
+        });
+    }
+
+    @Get("/absences")
+    @SecuredAction(value="Viescolaire.absences.view")
+    public void viewAbsences(final HttpServerRequest request){
+        UserUtils.getUserInfos(eb, request, new Handler<UserInfos>() {
+            @Override
+            public void handle(UserInfos user) {
+                if(user.getType().equals("Teacher")) {
+                    renderView(request, null, "absences/absc_teacher.html", null);
+                }else if(user.getType().equals("Personnel")){
+                    renderView(request, null, "absences/absc_personnel.html", null);
+                }
             }
         });
     }

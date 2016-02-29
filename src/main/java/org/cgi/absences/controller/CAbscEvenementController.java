4f54117b8689f9870edb10f994c00b98a51a2a19
lugana <anthony.lugan@cgi.com>
2016-02-29 17:40:15 +0100

    Absences enseignant

--- src/main/java/org/cgi/absences/controller/CAbscEvenementController.java
+++ src/main/java/org/cgi/absences/controller/CAbscEvenementController.java
@@ -12,13 +12,11 @@ import org.entcore.common.controller.ControllerHelper;
 import org.entcore.common.user.UserInfos;
 import org.entcore.common.user.UserUtils;
 import org.vertx.java.core.Handler;
-import org.vertx.java.core.eventbus.Message;
 import org.vertx.java.core.http.HttpServerRequest;
 import org.vertx.java.core.json.JsonArray;
 import org.vertx.java.core.json.JsonObject;
 
 import java.text.SimpleDateFormat;
-import java.util.ArrayList;
 import java.util.Date;
 
 import static org.entcore.common.http.response.DefaultResponseHandler.*;

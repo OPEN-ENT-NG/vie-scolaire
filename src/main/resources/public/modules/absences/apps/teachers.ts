c8be47ee6ffa940c443f9b6afe473472eeb98242
LEDUNOIS Simon <simon.ledunois@cgi.com>
2016-09-15 18:07:03 +0200

    Adding directives

--- src/main/resources/public/modules/absences/apps/teachers.ts
+++ src/main/resources/public/modules/absences/apps/teachers.ts
@@ -4,13 +4,16 @@
 import { notify, idiom as lang, template, ui, http, routes, ng } from '../../entcore/entcore';
 import {AbsencePrev, Appel, Cours, Creneau, Eleve, Evenement, Plage, VieScolaire, vieScolaire} from '../models/absc_enseignant_mdl';
 import {absencesController} from '../controllers/absc_enseignant_ctrl';
+import {mobilePanel} from '../../utils/directives/absences/mobile-panel';
 
 ng.controllers.push(absencesController);
 
+ng.directives.push(mobilePanel);
+
 routes.define(function($routeProvider){
     $routeProvider
-        .when('/viescolaire/absences/appel',{action:'appel'})
+        .when('/appel',{action:'appel'})
         .otherwise({
-            redirectTo : '/viescolaire/absences/appel'
+            redirectTo : '/appel'
         });
 });

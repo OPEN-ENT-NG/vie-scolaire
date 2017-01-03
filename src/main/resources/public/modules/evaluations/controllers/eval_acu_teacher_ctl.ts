2df03721fbac685794a2a8c53e5537b39f13d9e9
anabah <henri.anaba@cgi.com>
2017-01-03 11:13:22 +0100

    [MN-145]: filtrer les devoirs non évalués.

--- src/main/resources/public/modules/evaluations/controllers/eval_acu_teacher_ctl.ts
+++ src/main/resources/public/modules/evaluations/controllers/eval_acu_teacher_ctl.ts
@@ -8,13 +8,17 @@ declare let _:any;
 export let evalAcuTeacherController = ng.controller('EvalAcuTeacherController', [
     '$scope', 'route', 'model',
     function ($scope, route, model) {
+        $scope.evaluations = evaluations;
+        $scope.devoirs= [];
 
         // TODO chercher ses evaluations non terminees
-        $scope.evaluations = evaluations;
-        $scope.devoirs = evaluations.devoirs;
-
-
-
+        $scope.evaluations.devoirs.getPercentDone();
+        for(var i=0; i< $scope.evaluations.devoirs.all.length; i++){
+            if($scope.evaluations.devoirs.all[i].percent != 100){
+                $scope.devoirs.push($scope.evaluations.devoirs.all[i]);
+                utils.safeApply($scope);
+            }
+        }
         $scope.getDateFormated = function (date) {
             return utils.getFormatedDate(date, "DD/MM/YYYY");
         };

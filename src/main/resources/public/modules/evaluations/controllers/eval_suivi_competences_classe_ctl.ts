c81d5a134f5efb2b2af4eafbbc72934473c699f8
LEDUNOIS Simon <simon.ledunois@cgi.com>
2017-01-06 17:09:51 +0100

    [Suivi Classe] : Résolution bug model.me

--- src/main/resources/public/modules/evaluations/controllers/eval_suivi_competences_classe_ctl.ts
+++ src/main/resources/public/modules/evaluations/controllers/eval_suivi_competences_classe_ctl.ts
@@ -2,7 +2,7 @@
  * Created by ledunoiss on 27/10/2016.
  */
 
-import {ng, template } from 'entcore/entcore';
+import {ng, template, model} from 'entcore/entcore';
 import {SuiviCompetence, Domaine,SuiviCompetenceClasse} from '../models/eval_teacher_mdl';
 import * as utils from '../utils/teacher';
 

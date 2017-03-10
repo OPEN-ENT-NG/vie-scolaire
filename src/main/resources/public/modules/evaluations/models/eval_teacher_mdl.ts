daedd3f6af524ab4c395fc1d01149c716dad3350
vogelmt <matthieu.vogel@cgi.com>
2017-03-10 15:01:55 +0100

    MN-285 : [Suivi Eleve] - Vidage du graphe des devoirs lors de la création d'une éval libre

--- src/main/resources/public/modules/evaluations/models/eval_teacher_mdl.ts
+++ src/main/resources/public/modules/evaluations/models/eval_teacher_mdl.ts
@@ -1303,7 +1303,7 @@ export class SuiviCompetenceClasse extends Model implements IModel{
     findCompetence (idCompetence) {
         for(var i=0; i<this.domaines.all.length; i++) {
             var comp = findCompetenceRec(idCompetence, this.domaines.all[i].competences);
-            if(comp !== undefined) {
+            if(comp !== undefined && comp !== false) {
                 return comp;
             } else {
                 continue;
@@ -1405,7 +1405,7 @@ export class SuiviCompetence extends Model implements IModel{
     findCompetence (idCompetence) {
         for(var i=0; i<this.domaines.all.length; i++) {
             var comp = findCompetenceRec(idCompetence, this.domaines.all[i]);
-            if(comp !== undefined) {
+            if(comp !== undefined && comp !== false) {
                 return comp;
             } else {
                 continue;

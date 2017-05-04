55cf06ce53a1a46659d35abe64f04161c15ee227
vogelmt <matthieu.vogel@cgi.com>
2017-05-04 12:01:35 +0200

    MA-100 : Refondre Modèle personnel : Absences non justifiées

--- src/main/resources/public/modules/absences/models/personnel/Enseignant.ts
+++ src/main/resources/public/modules/absences/models/personnel/Enseignant.ts
@@ -1,3 +1,7 @@
 import { DefaultEnseignant } from '../common/DefaultEnseignant';
 
-export class Enseignant extends DefaultEnseignant {}
+export class Enseignant extends DefaultEnseignant {
+    firstName: string;
+    lastName: string;
+    selected: boolean;
+}

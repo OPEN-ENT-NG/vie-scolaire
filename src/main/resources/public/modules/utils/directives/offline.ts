a2a867054529c23f5d3649ef83530dda8c8124a6
LEDUNOIS Simon <simon.ledunois@cgi.com>
2017-05-09 16:44:26 +0200

    MA-90 : Gérer la perte de connexion internet

--- src/main/resources/public/modules/utils/directives/offline.ts
+++ src/main/resources/public/modules/utils/directives/offline.ts
@@ -1,21 +1,43 @@
-import { ng } from 'entcore/entcore';
+import { ng, idiom as lang } from 'entcore/entcore';
+import { safeApply } from '../functions/safeApply';
 
 export let offline = ng.directive('offline', () => {
     return {
         restrict : 'E',
         transclude : true,
+        template: '<div class="offline-message card {{notification.status}}" ng-if="notification !== undefined">' +
+        '<p><i class="horizontal-spacing"></i>[[getI18nMessage()]]</p>' +
+        '</div>',
         controller : ['$scope', '$rootScope', '$window', ($scope, $rootScope, $window) => {
             $rootScope.online = navigator.onLine;
+
+            /**
+             * Return the i18n value based on notification status
+             */
+            $scope.getI18nMessage = function () {
+                return lang.translate('viescolaire.connection.' + $scope.notification.status);
+            };
+
             $window.addEventListener('offline', () => {
                 $rootScope.$apply(() => {
-                    $rootScope.online = false;
-                    alert('OFFLINE');
+                    $scope.notification = {
+                        status: 'offline',
+                    };
+                    setTimeout(function () {
+                        if ($scope.hasOwnProperty('notification')) {
+                            $scope.notification.status = 'attempting';
+                            safeApply($scope);
+                        }
+                    }, 3000);
                 }, false);
             });
             $window.addEventListener('online', () => {
                 $rootScope.$apply(() => {
-                    $rootScope.online = true;
-                    alert('ONLINE');
+                    $scope.notification.status = 'online';
+                    setTimeout(function () {
+                        delete $scope.notification;
+                        safeApply($scope);
+                    }, 3000);
                 }, false);
             }, false);
         }]

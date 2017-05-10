6bacebb3fe413c676940b4e0bff898025f8158ec
vogelmt <matthieu.vogel@cgi.com>
2017-05-10 17:05:34 +0200

    MA-91 : Supprimer le référentiel utilisateur

--- src/main/java/fr/openent/viescolaire/service/impl/DefaultCoursService.java
+++ src/main/java/fr/openent/viescolaire/service/impl/DefaultCoursService.java
@@ -71,13 +71,9 @@ public class DefaultCoursService extends SqlCrudService implements CoursService 
 
         query.append("SELECT cours.*, to_char(cours.timestamp_dt, 'HH24:MI') as heure_debut ")
                 .append("FROM "+ Viescolaire.VSCO_SCHEMA +".cours ")
-                .append("WHERE id_personnel::varchar = ? ")
-                //.append("AND personnel.id = rel_personnel_cours.fk_personnel_id ")
-                //.append("AND rel_personnel_cours.fk_cours_id = cours.id ")
-                .append("AND to_date(?, 'DD-MM-YYYY') < cours.timestamp_dt ")
-                .append("AND cours.timestamp_fn < to_date(?, 'DD-MM-YYYY') ")
-               // .append("AND cours.fk_classe_id = classe.id ")
-
+                .append("WHERE id_personnel = ? ")
+                .append("AND to_timestamp(?, 'YYYY-MM-DD HH24:MI:SS') < cours.timestamp_dt ")
+                .append("AND cours.timestamp_fn < to_timestamp(?, 'YYYY-MM-DD HH24:MI:SS') ")
                 .append("ORDER BY cours.timestamp_dt ASC");
 
         values.addString(psUserId);

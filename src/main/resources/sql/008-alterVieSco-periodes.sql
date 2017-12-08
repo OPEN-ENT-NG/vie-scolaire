ebbf913a57a32eb097bda8a386837e160e2bca87
vogelmt <matthieu.vogel@cgi.com>
2017-12-08 11:47:30 +0100

    Script Initialisation : Période (FIX)

--- src/main/resources/sql/008-alterVieSco-periodes.sql
+++ src/main/resources/sql/008-alterVieSco-periodes.sql
@@ -1,15 +1,3 @@
-ALTER TABLE viesco.periode
-    ADD COLUMN id_classe character(36) NOT NULL,
-    ADD COLUMN id_type bigint NOT NULL,
-    ALTER timestamp_dt SET NOT NULL,
-    ALTER timestamp_fn SET NOT NULL,
-    ALTER date_fin_saisie SET NOT NULL,
-    DROP COLUMN libelle,
-    ADD CONSTRAINT fk_type_periode FOREIGN KEY (id_type)
-    REFERENCES viesco.rel_type_periode (id) MATCH SIMPLE
-    ON UPDATE NO ACTION ON DELETE NO ACTION;
-
-
 CREATE TABLE viesco.rel_type_periode
 (
     id BIGSERIAL NOT NULL,
@@ -20,6 +8,16 @@ CREATE TABLE viesco.rel_type_periode
     CONSTRAINT type_ordre_unique UNIQUE (type, ordre)
 );
 
+ALTER TABLE viesco.periode
+    ADD COLUMN id_classe character(36) NOT NULL,
+    ADD COLUMN id_type bigint NOT NULL,
+    ALTER timestamp_dt SET NOT NULL,
+    ALTER timestamp_fn SET NOT NULL,
+    ALTER date_fin_saisie SET NOT NULL,
+    DROP COLUMN libelle,
+    ADD CONSTRAINT fk_type_periode FOREIGN KEY (id_type)
+    REFERENCES viesco.rel_type_periode (id) MATCH SIMPLE
+    ON UPDATE NO ACTION ON DELETE NO ACTION;
 
 INSERT INTO viesco.rel_type_periode (type, ordre) VALUES (2, 1);
 INSERT INTO viesco.rel_type_periode (type, ordre) VALUES (2, 2);

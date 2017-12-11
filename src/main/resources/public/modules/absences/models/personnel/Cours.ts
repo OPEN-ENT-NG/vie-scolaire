65d734f9a4177d647e9201a92c570896c5be7f5a
RAHNI Rhita <rhita.rahni@logica.com>
2017-12-11 17:52:19 +0100

    [MA-162]-Sélection créneau [MA-163]-IHM Calendrier [MA-165]-Modification récupération cours ( saisie d'absence) [MA-170]-Performances page accueil CPE présences

--- src/main/resources/public/modules/absences/models/personnel/Cours.ts
+++ src/main/resources/public/modules/absences/models/personnel/Cours.ts
@@ -9,4 +9,62 @@ import { Model } from 'entcore';
 export class Cours extends DefaultCours implements Model{
     etatcours: string;
     id_appel: number;
+    _id: string;
+    _occurenceId?: string;
+    structureId: string;
+    startDate: string | Object;
+    endDate: string | Object;
+    dayOfWeek: number;
+    teacherIds: string[];
+    subjectId: string;
+    roomLabels: string[];
+    classes: string[];
+    groups: string[];
+    color: string;
+    is_periodic: boolean;
+    startMoment: any;
+    startMomentDate: string;
+    startMomentTime: string;
+    startCalendarHour: Date;
+    endCalendarHour: Date;
+    endMoment: any;
+    endMomentDate: string;
+    endMomentTime: string;
+    subjectLabel: string;
+    courseOccurrences: CourseOccurrence[];
+    teachers: any;
+    enseignantName : string[];
+    originalStartMoment?: any;
+    originalEndMoment?: any;
+
+
+    constructor (obj: Object, startDate?: string | Object, endDate?: string | Object) {
+        super();
+        if (obj instanceof Object) {
+            for (let key in obj) {
+                this[key] = obj[key];
+            }
+        }
+        this.is_periodic = false;
+        if (startDate) {
+            this.startMoment = moment(startDate);
+            this.startCalendarHour = this.startMoment.seconds(0).millisecond(0).toDate();
+            this.startMomentDate = this.startMoment.format('DD/MM/YYYY');
+            this.startMomentTime = this.startMoment.format('hh:mm');
+        }
+        if (endDate) {
+            this.endMoment = moment(endDate);
+            this.endCalendarHour = this.endMoment.seconds(0).millisecond(0).toDate();
+            this.endMomentDate = this.endMoment.format('DD/MM/YYYY');
+            this.endMomentTime = this.endMoment.format('hh:mm');
+        }
+    }
+
+
+}
+export class CourseOccurrence {
+    dayOfWeek: any;
+    startTime: Date;
+    endTime: Date;
+    roomLabels: string[];
 }

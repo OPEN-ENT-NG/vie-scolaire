e254cb25796d23519f4dcbe90f70031e0dca114d
agnes.lapeyronnie <agnes.lapeyronnie@cgi.com>
2017-12-26 17:55:32 +0100

    ELL-12 :  Retour LSU sprint1.

--- src/main/resources/public/modules/evaluations/models/common/LSU.ts
+++ src/main/resources/public/modules/evaluations/models/common/LSU.ts
@@ -6,14 +6,14 @@ import {Responsable, Classe, Structure} from '../eval_teacher_mdl';
 
 
 export class LSU {
-    responsables :Collection<Responsable>;
-    classes : Collection<Classe>;
+    responsables : Collection<Responsable>;
+    classes : Array<Classe>;//sans les groupes
     structureId : string;
 
 
-    constructor (structureId : string, classes : Collection<Classe>, responsables : Collection<Responsable>){
+    constructor (structureId : string, classes : Array<Classe>, responsables : Collection<Responsable>){
         this.structureId = structureId ;
-        this.classes = _.clone(classes);
+        this.classes = classes;
         this.responsables =_.clone(responsables) ;
 
     }
@@ -21,13 +21,13 @@ export class LSU {
     export () {
         let url = "/viescolaire/evaluations/exportLSU/lsu?idStructure=" + this.structureId;
 
-            for(var i=0; i<this.classes.all.length;i++) {
-                if(this.classes.all[i].selected){
-                    url += "&idClasse=" + this.classes.all[i].id;
+            for(var i=0; i<this.classes.length;i++) {
+                if(this.classes[i].selected){
+                    url += "&idClasse=" + this.classes[i].id;
                 }
             }
 
-         /*   _.each(_.where(this.classes.all, {selected: true}), (classe) => {
+          /*  _.each(_.where(this.classes.all, {selected: true}), (classe) => {
                     url += "&idClasse=" + this.classes.all[i].id;
 
             });*/

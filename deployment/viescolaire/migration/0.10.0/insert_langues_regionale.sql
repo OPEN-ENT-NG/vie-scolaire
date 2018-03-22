db64dc746d1b460c0c3a70acdbce9bb5f711dc79
lugana <anthony.lugan@cgi.com>
2018-03-22 14:13:53 +0100

    MN-554 : fix changement des données

--- deployment/viescolaire/migration/0.10.0/insert_langues_regionale.sql
+++ deployment/viescolaire/migration/0.10.0/insert_langues_regionale.sql
@@ -1,6 +1,7 @@
-INSERT INTO notes.langues_culture_regionale(libelle, code) VALUES ('Basque', 'BAS');
+INSERT INTO notes.langues_culture_regionale(libelle, code) VALUES ('Aucun', 'AUC');
+INSERT INTO notes.langues_culture_regionale(libelle, code) VALUES ('Basque', 'BAQ');
 INSERT INTO notes.langues_culture_regionale(libelle, code) VALUES ('Breton', 'BRE');
-INSERT INTO notes.langues_culture_regionale(libelle, code) VALUES ('Corse', 'COR');
-INSERT INTO notes.langues_culture_regionale(libelle, code) VALUES ('Occitan langue d''oc', 'OCC');
-INSERT INTO notes.langues_culture_regionale(libelle, code) VALUES ('Langue régionale d''Alsace', 'ALS');
-INSERT INTO notes.langues_culture_regionale(libelle, code) VALUES ('Langue régionale des pays mosellans', 'MOS');
+INSERT INTO notes.langues_culture_regionale(libelle, code) VALUES ('Catalan', 'CAT');
+INSERT INTO notes.langues_culture_regionale(libelle, code) VALUES ('Corse', 'COS');
+INSERT INTO notes.langues_culture_regionale(libelle, code) VALUES ('Langue régionale d''Alsace', 'GSW');
+INSERT INTO notes.langues_culture_regionale(libelle, code) VALUES ('Occitan langue d''oc', 'OCI');

d9af3d241a35dde51fcd9d17c864854c4a57e118
anabah <henri.anaba@cgi.com>
2018-04-12 14:19:44 +0200

    [MN-4 | MN-480] Colonne Appréciation dans le relevé périodique.

---
+++ src/main/resources/sql/021-alterNotes-appreciationMatierePeriode.sql
@@ -0,0 +1,13 @@
+CREATE TABLE notes.appreciation_matiere_periode
+(
+    id_periode bigint NOT NULL,
+    id_eleve character varying COLLATE pg_catalog."default" NOT NULL,
+    appreciation_matiere_periode character varying(300) NOT NULL,
+    id_classe character varying COLLATE pg_catalog."default" NOT NULL,
+    id_matiere character varying COLLATE pg_catalog."default" NOT NULL,
+    CONSTRAINT pk_appreciation_matiere_periode PRIMARY KEY (id_periode, id_classe, id_matiere, id_eleve),
+    CONSTRAINT fk_id_periode FOREIGN KEY (id_periode)
+        REFERENCES viesco.periode (id) MATCH SIMPLE
+        ON UPDATE CASCADE
+        ON DELETE CASCADE
+);

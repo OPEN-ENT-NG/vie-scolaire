e6456aa1932e97530210d38d2d7c350033a0c5ef
JRob <jeremy.robertou@gmail.com>
2018-04-12 15:46:07 +0200

    Fix merge (split-presence & split)

--- src/main/java/fr/openent/DisplayController.java
+++ src/main/java/fr/openent/DisplayController.java
@@ -23,9 +23,6 @@ import fr.wseduc.rs.ApiDoc;
 import fr.wseduc.rs.Get;
 import fr.wseduc.security.SecuredAction;
 import org.entcore.common.controller.ControllerHelper;
-import org.entcore.common.user.UserInfos;
-import org.entcore.common.user.UserUtils;
-import org.vertx.java.core.Handler;
 import org.vertx.java.core.http.HttpServerRequest;
 
 public class DisplayController extends ControllerHelper {
@@ -36,18 +33,8 @@ public class DisplayController extends ControllerHelper {
 
     @Get(Viescolaire.VSCO_PATHPREFIX)
     @ApiDoc("Get Vie Scolaire HTML view")
-    @SecuredAction(value="Viescolaire.view")
-    public void view(final HttpServerRequest request){
-        UserUtils.getUserInfos(eb, request, new Handler<UserInfos>() {
-
-            @Override
-            public void handle(UserInfos user) {
-                if(user.getType().equals("Teacher")) {
-                    renderView(request, null, "vsco_teacher.html", null);
-                }else if(user.getType().equals("Personnel")){
-                    renderView(request, null, "vsco_personnel.html", null);
-                }
-            }
-        });
+    @SecuredAction(value = "Viescolaire.view")
+    public void view(final HttpServerRequest request) {
+        renderView(request, null, "viescolaire/vsco_personnel.html", null);
     }
 }

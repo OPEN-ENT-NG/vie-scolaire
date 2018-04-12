dab1a8f252a54b0cbe36fb7e9326786b87c2ae5c
vogelmt <matthieu.vogel@cgi.com>
2018-04-12 18:28:28 +0200

    MN-585 : en tant qu'enseignant je veux utiliser les outils d'aide à la saisie du Ministère pour les éléments du programme travaillés

--- src/main/resources/sql/018-appreciation-classes.sql
+++ src/main/resources/sql/018-appreciation-classes.sql
@@ -4,5 +4,6 @@ CREATE TABLE notes.appreciation_classe
   id_classe character varying(36) NOT NULL,
   id_periode bigint NOT NULL,
   id_matiere character varying(36) NOT NULL,
-  CONSTRAINT appreciation_classe_pk PRIMARY KEY (id_classe, id_periode, id_matiere)
+  CONSTRAINT appreciation_classe_pk PRIMARY KEY (id_classe, id_periode, id_matiere),
+  CONSTRAINT fk_periode_id FOREIGN KEY (id_periode) REFERENCES viesco.periode (id) MATCH SIMPLE ON UPDATE CASCADE ON DELETE CASCADE
 );

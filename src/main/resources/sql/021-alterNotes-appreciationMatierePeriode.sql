f6fc6d7f2c0d954b5cd0e99cd408de47f4bbcaee
vogelmt <matthieu.vogel@cgi.com>
2018-04-26 13:38:21 +0200

    MN-4, MN-480 : refactor SQL

--- src/main/resources/sql/021-alterNotes-appreciationMatierePeriode.sql
+++ src/main/resources/sql/021-alterNotes-appreciationMatierePeriode.sql
@@ -1,10 +1,10 @@
 CREATE TABLE notes.appreciation_matiere_periode
 (
     id_periode bigint NOT NULL,
-    id_eleve character varying COLLATE pg_catalog."default" NOT NULL,
+    id_eleve character varying NOT NULL,
     appreciation_matiere_periode character varying(300) NOT NULL,
-    id_classe character varying COLLATE pg_catalog."default" NOT NULL,
-    id_matiere character varying COLLATE pg_catalog."default" NOT NULL,
+    id_classe character varying NOT NULL,
+    id_matiere character varying NOT NULL,
     CONSTRAINT pk_appreciation_matiere_periode PRIMARY KEY (id_periode, id_classe, id_matiere, id_eleve),
     CONSTRAINT fk_id_periode FOREIGN KEY (id_periode)
         REFERENCES viesco.periode (id) MATCH SIMPLE

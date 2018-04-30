5494b2e7a07c2c8cba41655bb7ffd871f38fac55
agnes.lapeyronnie <agnes.lapeyronnie@cgi.com>
2018-04-30 17:13:12 +0200

    [MN-568]:Fix niveau_enseignement_complement : Script SQL

--- src/main/resources/sql/025-niveauEnseignementCompement.sql
+++ src/main/resources/sql/025-niveauEnseignementCompement.sql
@@ -10,10 +10,16 @@ CREATE TABLE notes.niveau_ens_complement
 
 INSERT INTO notes.niveau_ens_complement (id,libelle,niveau,bareme_brevet)
 VALUES (1,'Objectif atteint',1,10),(2,'Objectif dépassé',2,20);
+INSERT INTO notes.niveau_ens_complement(id, libelle, niveau, bareme_brevet)
+	VALUES (3, 'aucun', 0, 0);
 
 ALTER TABLE notes.niveau_competences ADD bareme_brevet int ;
 
-UPDATE notes.niveau_competences SET bareme_brevet = ordre * 10;
+UPDATE notes.niveau_competences SET bareme_brevet = 10 where ordre = 1;
+UPDATE notes.niveau_competences SET bareme_brevet = 25 where ordre = 2;
+UPDATE notes.niveau_competences SET bareme_brevet = 40 where ordre = 3;
+UPDATE notes.niveau_competences SET bareme_brevet = 50 where ordre = 4;
+
 
 ALTER TABLE notes.eleve_enseignement_complement RENAME COLUMN "niveau" TO "id_niveau";
 ALTER TABLE notes.eleve_enseignement_complement ADD CONSTRAINT fk_niveau_id FOREIGN KEY(id_niveau)

a65179a78881c7e3994927272d0db07f112beef1
anabah <henri.anaba@cgi.com>
2018-07-17 12:44:54 +0200

    [Fix MN-663]: Lenteur récupération des élèves dans une classe.

--- src/main/java/fr/openent/viescolaire/service/impl/DefaultClasseService.java
+++ src/main/java/fr/openent/viescolaire/service/impl/DefaultClasseService.java
@@ -87,12 +87,17 @@ public class DefaultClasseService extends SqlCrudService implements ClasseServic
                 .append(RETURNING)
                 .append(" UNION MATCH (u:User {profiles: ['Student']})-[:IN]-(c:ManualGroup {id :{idClasse}}) ")
                 .append(RETURNING)
-                .append(" UNION MATCH (dg:DeleteGroup)<-[:IN]-(u:User {profiles: ['Student']})-[:HAS_RELATIONSHIPS]->(b:Backup) ,")
-                .append(" (g:Group), (c:Class) ")
+                .append(" UNION MATCH (u:User {profiles: ['Student']})-[:HAS_RELATIONSHIPS]->(b:Backup),")
+                .append(" (c:Group)")
                 .append(" WHERE HAS(u.deleteDate) ")
-                .append(" AND  c.externalId IN u.classes ")
                 .append(" AND (c.id = {idClasse} ")
-                .append("      OR (g.id = {idClasse}  AND (g.externalId IN u.groups OR g.id IN b.IN_OUTGOING) )) ")
+                .append(" AND (c.externalId IN u.groups OR c.id IN b.IN_OUTGOING) ) ")
+                .append(RETURNING)
+                .append(" UNION MATCH (u:User {profiles: ['Student']})-[:HAS_RELATIONSHIPS]->(b:Backup),")
+                .append(" (c:Class)")
+                .append(" WHERE HAS(u.deleteDate) ")
+                .append(" AND (c.id = {idClasse} ")
+                .append(" AND  c.externalId IN u.classes) ")
                 .append(RETURNING);
 
         String [] sortedField = new  String[2];
@@ -259,12 +264,17 @@ public class DefaultClasseService extends SqlCrudService implements ClasseServic
                 .append(" UNION MATCH (u:User {profiles: ['Student']})-[:IN]-(c:ManualGroup) ")
                 .append(" WHERE c.id IN {idClasses} ")
                 .append(RETURNING)
-                .append(" UNION MATCH (dg:DeleteGroup)<-[:IN]-(u:User{profiles: ['Student']})-[:HAS_RELATIONSHIPS]->(b:Backup),")
-                .append(" (g:Group), (c:Class) ")
+                .append(" UNION MATCH (u:User {profiles: ['Student']})-[:HAS_RELATIONSHIPS]->(b:Backup), ")
+                .append(" (c:Group)")
                 .append(" WHERE HAS(u.deleteDate) ")
-                .append(" AND c.externalId IN u.classes ")
                 .append(" AND (c.id IN {idClasses} ")
-                .append("      OR (g.id IN {idClasses} AND (g.externalId IN u.groups  OR g.id IN b.IN_OUTGOING ) ) ) ")
+                .append(" AND (c.externalId IN u.groups OR c.id IN b.IN_OUTGOING) ) ")
+                .append(RETURNING)
+                .append(" UNION MATCH (u:User {profiles: ['Student']})-[:HAS_RELATIONSHIPS]->(b:Backup),")
+                .append(" (c:Class)")
+                .append(" WHERE HAS(u.deleteDate) ")
+                .append(" AND (c.id IN {idClasses} ")
+                .append(" AND  c.externalId IN u.classes) ")
                 .append(RETURNING);
 
         params.put("idClasses", new fr.wseduc.webutils.collections.JsonArray(Arrays.asList(idClasses)));

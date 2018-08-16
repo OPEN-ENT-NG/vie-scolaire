fbcecd0a04a5e1c69aa0e1b2106a122c45e5e25a
ursulejaky.maffomako <Valence123>
2018-08-16 14:58:27 +0200

    [MN-400][MN-402] ajout collone personnalise à la table thematique_bilan_periodique

--- src/main/resources/sql/036-elements-bilan-periodique.sql
+++ src/main/resources/sql/036-elements-bilan-periodique.sql
@@ -22,19 +22,19 @@ CREATE TABLE notes.thematique_bilan_periodique (
   ON UPDATE NO ACTION ON DELETE NO ACTION
 );
 
-INSERT INTO notes.thematique_bilan_periodique(libelle, code, type_elt_bilan_periodique) VALUES ('Parcours avenir', 'PAR_AVN', 3);
-INSERT INTO notes.thematique_bilan_periodique(libelle, code, type_elt_bilan_periodique) VALUES ('Parcours citoyen', 'PAR_CIT', 3);
-INSERT INTO notes.thematique_bilan_periodique(libelle, code, type_elt_bilan_periodique) VALUES ('Parcours d''éducation artistique et culturelle', 'PAR_ART', 3);
-INSERT INTO notes.thematique_bilan_periodique(libelle, code, type_elt_bilan_periodique) VALUES ('Parcours éducatif de santé', 'PAR_SAN', 3);
+INSERT INTO notes.thematique_bilan_periodique(libelle, code, type_elt_bilan_periodique, personnalise) VALUES ('Parcours avenir', 'PAR_AVN', 3, false);
+INSERT INTO notes.thematique_bilan_periodique(libelle, code, type_elt_bilan_periodique, personnalise) VALUES ('Parcours citoyen', 'PAR_CIT', 3, false);
+INSERT INTO notes.thematique_bilan_periodique(libelle, code, type_elt_bilan_periodique, personnalise) VALUES ('Parcours d''éducation artistique et culturelle', 'PAR_ART', 3, false);
+INSERT INTO notes.thematique_bilan_periodique(libelle, code, type_elt_bilan_periodique, personnalise) VALUES ('Parcours éducatif de santé', 'PAR_SAN', 3, false);
 
-INSERT INTO notes.thematique_bilan_periodique(libelle, code, type_elt_bilan_periodique) VALUES ('Corps, santé, bien-être et sécurité', 'EPI_SAN', 1);
-INSERT INTO notes.thematique_bilan_periodique(libelle, code, type_elt_bilan_periodique) VALUES ('Culture et création artistiques', 'EPI_ART', 1);
-INSERT INTO notes.thematique_bilan_periodique(libelle, code, type_elt_bilan_periodique) VALUES ('Transition écologique et développement durable', 'EPI_EDD', 1);
-INSERT INTO notes.thematique_bilan_periodique(libelle, code, type_elt_bilan_periodique) VALUES ('Information, communication, citoyenneté', 'EPI_EDD', 1);
-INSERT INTO notes.thematique_bilan_periodique(libelle, code, type_elt_bilan_periodique) VALUES ('Langues et cultures de l''Antiquité', 'EPI_LGA', 1);
-INSERT INTO notes.thematique_bilan_periodique(libelle, code, type_elt_bilan_periodique) VALUES ('Langues et cultures étrangères ou, le cas échéant, régionales', 'EPI_LGE', 1);
-INSERT INTO notes.thematique_bilan_periodique(libelle, code, type_elt_bilan_periodique) VALUES ('Monde économique et professionnel', 'EPI_PRO', 1);
-INSERT INTO notes.thematique_bilan_periodique(libelle, code, type_elt_bilan_periodique) VALUES ('Sciences, technologie et société', 'EPI_STS', 1);
+INSERT INTO notes.thematique_bilan_periodique(libelle, code, type_elt_bilan_periodique) VALUES ('Corps, santé, bien-être et sécurité', 'EPI_SAN', 1, false);
+INSERT INTO notes.thematique_bilan_periodique(libelle, code, type_elt_bilan_periodique) VALUES ('Culture et création artistiques', 'EPI_ART', 1, false);
+INSERT INTO notes.thematique_bilan_periodique(libelle, code, type_elt_bilan_periodique) VALUES ('Transition écologique et développement durable', 'EPI_EDD', 1, false);
+INSERT INTO notes.thematique_bilan_periodique(libelle, code, type_elt_bilan_periodique) VALUES ('Information, communication, citoyenneté', 'EPI_EDD', 1, false);
+INSERT INTO notes.thematique_bilan_periodique(libelle, code, type_elt_bilan_periodique) VALUES ('Langues et cultures de l''Antiquité', 'EPI_LGA', 1, false);
+INSERT INTO notes.thematique_bilan_periodique(libelle, code, type_elt_bilan_periodique) VALUES ('Langues et cultures étrangères ou, le cas échéant, régionales', 'EPI_LGE', 1, false);
+INSERT INTO notes.thematique_bilan_periodique(libelle, code, type_elt_bilan_periodique) VALUES ('Monde économique et professionnel', 'EPI_PRO', 1, false);
+INSERT INTO notes.thematique_bilan_periodique(libelle, code, type_elt_bilan_periodique) VALUES ('Sciences, technologie et société', 'EPI_STS', 1, false);
 
 CREATE TABLE notes.elt_bilan_periodique (
   id bigserial NOT NULL,

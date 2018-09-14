8ed94326c3066e747846ec46a172dd9165245c0b
lapeyronnie <agnes.lapeyronnie@cgi.com>
2018-09-14 10:59:05 +0200

    [MN-175] : create table competence_niveau_final without id_matiere

--- src/main/resources/sql/037-competenceNiveauFinal.sql
+++ src/main/resources/sql/037-competenceNiveauFinal.sql
@@ -5,13 +5,12 @@ CREATE TABLE notes.competence_niveau_final
 	niveau_final integer,
 	id_competence bigint NOT NULL,
 	id_matiere character varying(255) NOT NULL,
-	id_classe character varying(255) NOT NULL,
-	CONSTRAINT pk_niveau_final PRIMARY KEY (id_periode, id_eleve, id_competence, id_matiere, id_classe),
+	CONSTRAINT pk_niveau_final PRIMARY KEY (id_periode, id_eleve, id_competence, id_matiere),
 	CONSTRAINT fk_periode_id FOREIGN KEY (id_periode)
 	REFERENCES viesco.rel_type_periode (id) MATCH SIMPLE
 	ON UPDATE CASCADE ON DELETE CASCADE,
 	CONSTRAINT fk_competences_id FOREIGN KEY (id_competence)
 	REFERENCES notes.competences (id) MATCH SIMPLE
 	ON UPDATE CASCADE ON DELETE CASCADE,
-	CONSTRAINT competence_niveau_final UNIQUE (id_periode, id_eleve, id_competence, id_matiere, id_classe)
+	CONSTRAINT competence_niveau_final UNIQUE (id_periode, id_eleve, id_competence, id_matiere)
 	);

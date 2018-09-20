e3cac56443a51d4036872ad6717b67730aad52a4
vogelmt <matthieu.vogel@cgi.com>
2018-09-20 11:21:10 +0200

    MN-717 : Impossible d'enregistrer un Parcours sur groupe manuel

--- src/main/resources/sql/038-alter-thematique_bilan_periodique.sql
+++ src/main/resources/sql/038-alter-thematique_bilan_periodique.sql
@@ -1,1 +1,2 @@
 ALTER TABLE notes.thematique_bilan_periodique ADD COLUMN id_etablissement character varying(36);
+ALTER TABLE notes.rel_elt_bilan_periodique_groupe ALTER COLUMN externalid_groupe DROP NOT NULL;

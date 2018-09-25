7dd7b7f5efa27a8f0dd9e2fc2df75b4c2ed51033
anabah <henri.anaba@cgi.com>
2018-09-25 12:05:01 +0200

    [Fix] drop constrainte NOT NULL externalid_groupe.

---
+++ src/main/resources/sql/040-alter-externalId_groupe-Elements-bilan-periodique.sql
@@ -0,0 +1,3 @@
+ALTER TABLE notes.appreciation_elt_bilan_periodique_classe ALTER COLUMN externalid_groupe DROP NOT NULL;
+
+ALTER TABLE notes.rel_groupe_appreciation_elt_eleve ALTER COLUMN externalid_groupe DROP NOT NULL;

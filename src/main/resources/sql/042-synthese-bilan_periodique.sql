74765fa90c41afa7a90122c4fc6ebfea5227d31a
c4role <carole.hermouet@cgi.com>
2018-10-22 11:52:11 +0200

    [MN-100] Script synthèse

---
+++ src/main/resources/sql/042-synthese-bilan_periodique.sql
@@ -0,0 +1,8 @@
+CREATE TABLE notes.synthese_bilan_periodique
+(
+  synthese character varying(600),
+  id bigserial NOT NULL,
+  id_typePeriode bigint NOT NULL,
+  id_eleve character varying(255) NOT NULL,
+  CONSTRAINT synthese_bilan_periodique_pk PRIMARY KEY (id_typePeriode, id_eleve)
+);

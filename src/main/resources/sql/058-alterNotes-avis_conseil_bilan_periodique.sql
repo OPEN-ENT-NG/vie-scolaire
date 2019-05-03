9745e3d01a45b0329234c49b4be338442ffb2d5d
sjol <samuel.jollois@cgi.com>
2019-05-03 15:08:42 +0200

    [MN-938]:alter table for new rows Orientation avis

---
+++ src/main/resources/sql/058-alterNotes-avis_conseil_bilan_periodique.sql
@@ -0,0 +1,13 @@
+DELETE FROM notes.avis_conseil_bilan_periodique;
+
+INSERT INTO notes.avis_conseil_bilan_periodique(libelle, type_avis)
+VALUES ('Félicitations', 1),
+        ('Compliments', 1),
+        ('Encouragements', 1),
+        ('Félicitations à l’unanimité du conseil', 1),
+        ('Admis(e) en 2nd générale', 2),
+        ('Admis(e) en 2nd professionnelle', 2),
+        ('Admis(e) en CAP', 2),
+        ('Admis(e) en 4ème', 2),
+        ('Admis(e) en 5ème', 2),
+        ('Redoublement', 2);

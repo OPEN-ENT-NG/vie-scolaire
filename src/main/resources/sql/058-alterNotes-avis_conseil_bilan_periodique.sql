4f32a77f7a9be95818dbc88e03773c122042f9c2
sjol <samuel.jollois@cgi.com>
2019-05-06 09:45:58 +0200

    [MN-938]: insert id in list of avis conseil de classe

--- src/main/resources/sql/058-alterNotes-avis_conseil_bilan_periodique.sql
+++ src/main/resources/sql/058-alterNotes-avis_conseil_bilan_periodique.sql
@@ -1,14 +1,14 @@
 DELETE FROM notes.avis_conseil_bilan_periodique;
 
-INSERT INTO notes.avis_conseil_bilan_periodique(libelle, type_avis)
-VALUES ('Félicitations', 1),
-        ('Compliments', 1),
-        ('Encouragements', 1),
-        ('Félicitations à l’unanimité du conseil', 1),
-        ('Admis(e) en 2nd générale', 2),
-        ('Admis(e) en 2nd professionnelle', 2),
-        ('Admis(e) en CAP', 2),
-        ('Admis(e) en 3ème', 2),
-        ('Admis(e) en 4ème', 2),
-        ('Admis(e) en 5ème', 2),
-        ('Redoublement', 2);
+INSERT INTO notes.avis_conseil_bilan_periodique(id,libelle, type_avis)
+VALUES (1,'Félicitations', 1),
+        (2,'Compliments', 1),
+        (3,'Encouragements', 1),
+        (7,'Félicitations à l’unanimité du conseil', 1),
+        (4,'Admis(e) en 2nd générale', 2),
+        (5,'Admis(e) en 2nd professionnelle', 2),
+        (6,'Admis(e) en CAP', 2),
+        (8,'Admis(e) en 3ème', 2),
+        (9,'Admis(e) en 4ème', 2),
+        (10,'Admis(e) en 5ème', 2),
+        (11,'Redoublement', 2);

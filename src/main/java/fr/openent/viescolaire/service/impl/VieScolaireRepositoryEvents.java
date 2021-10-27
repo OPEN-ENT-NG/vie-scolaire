6b0e3bae15a7cb11b00aa802fec4f9efadfada97
Nabil Mansouri <nabil.mansouri.3@gmail.com>
2021-10-27 14:48:48 +0200

    fix: repository event interface changed

--- src/main/java/fr/openent/viescolaire/service/impl/VieScolaireRepositoryEvents.java
+++ src/main/java/fr/openent/viescolaire/service/impl/VieScolaireRepositoryEvents.java
@@ -48,7 +48,7 @@ public class VieScolaireRepositoryEvents implements RepositoryEvents {
     }
 
     @Override
-    public void exportResources(String s, String s1, JsonArray jsonArray, String s2, String s3, String s4, Handler<Boolean> handler) {
+    public void exportResources(boolean exportDocuments, boolean exportSharedResources, String exportId, String userId, JsonArray groups, String exportPath, String locale, String host, Handler<Boolean> handler) {
         log.info("[VieScolaireRepositoryEvents] : export resources event is not implemented");
     }
 

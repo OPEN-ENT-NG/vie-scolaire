fd73d3e95dc60ffba72472ed5734ed294653bebf
Flavien Martineau <45894263+flmartineau@users.noreply.github.com>
2022-02-01 15:55:21 +0100

    fix(presences): #MVS-49 Add tagIds field in CommonCourseService results

--- src/main/java/fr/openent/viescolaire/service/impl/DefaultCommonCoursService.java
+++ src/main/java/fr/openent/viescolaire/service/impl/DefaultCommonCoursService.java
@@ -60,12 +60,33 @@ public class DefaultCommonCoursService extends DBService implements CommonCoursS
     public final static String EDT_SCHEMA = "edt";
 
     private final EventBus eb;
-    private static final JsonObject KEYS = new JsonObject().put(COURSE_TABLE._id, 1).put(COURSE_TABLE.structureId, 1).put(COURSE_TABLE.subjectId, 1)
-            .put(COURSE_TABLE.roomLabels, 1).put(COURSE_TABLE.equipmentLabels, 1).put(COURSE_TABLE.teacherIds, 1).put(COURSE_TABLE.personnelIds, 1)
-            .put(COURSE_TABLE.classes, 1).put(COURSE_TABLE.groups, 1).put(COURSE_TABLE.dayOfWeek, 1).put(COURSE_TABLE.startDate, 1).put(COURSE_TABLE.endDate, 1)
-            .put(COURSE_TABLE.everyTwoWeek, 1).put(COURSE_TABLE.manual, 1).put(COURSE_TABLE.exceptionnal, 1).put(COURSE_TABLE.author, 1).put(COURSE_TABLE.lastUser, 1)
-            .put(COURSE_TABLE.created, 1).put(COURSE_TABLE.updated, 1).put(COURSE_TABLE.idStartSlot, 1).put(COURSE_TABLE.idEndSlot, 1).put(COURSE_TABLE.classesExternalIds, 1)
-            .put(COURSE_TABLE.groupsExternalIds, 1).put(COURSE_TABLE.recurrence, 1).put(COURSE_TABLE.timetableSubjectId, 1);
+    private static final JsonObject KEYS = new JsonObject()
+            .put(COURSE_TABLE._id, 1)
+            .put(COURSE_TABLE.structureId, 1)
+            .put(COURSE_TABLE.subjectId, 1)
+            .put(COURSE_TABLE.roomLabels, 1)
+            .put(COURSE_TABLE.equipmentLabels, 1)
+            .put(COURSE_TABLE.teacherIds, 1)
+            .put(COURSE_TABLE.tagIds, 1)
+            .put(COURSE_TABLE.personnelIds, 1)
+            .put(COURSE_TABLE.classes, 1)
+            .put(COURSE_TABLE.groups, 1)
+            .put(COURSE_TABLE.dayOfWeek, 1)
+            .put(COURSE_TABLE.startDate, 1)
+            .put(COURSE_TABLE.endDate, 1)
+            .put(COURSE_TABLE.everyTwoWeek, 1)
+            .put(COURSE_TABLE.manual, 1)
+            .put(COURSE_TABLE.exceptionnal, 1)
+            .put(COURSE_TABLE.author, 1)
+            .put(COURSE_TABLE.lastUser, 1)
+            .put(COURSE_TABLE.created, 1)
+            .put(COURSE_TABLE.updated, 1)
+            .put(COURSE_TABLE.idStartSlot, 1)
+            .put(COURSE_TABLE.idEndSlot, 1)
+            .put(COURSE_TABLE.classesExternalIds, 1)
+            .put(COURSE_TABLE.groupsExternalIds, 1)
+            .put(COURSE_TABLE.recurrence, 1)
+            .put(COURSE_TABLE.timetableSubjectId, 1);
     private static final String START_DATE_PATTERN = "T00:00Z";
     private static final String END_DATE_PATTERN = "T23.59Z";
     private static final String START_END_DATE_FORMAT = "yyyy-MM-dd'T'HH:mm:ss";
@@ -620,6 +641,7 @@ class Course {
     protected final String roomLabels = "roomLabels";
     protected final String equipmentLabels = " equipmentLabels";
     protected final String teacherIds = "teacherIds";
+    protected final String tagIds = "tagIds";
     protected final String personnelIds = "personnelIds";
     protected final String classes = "classes";
     protected final String groups = "groups";

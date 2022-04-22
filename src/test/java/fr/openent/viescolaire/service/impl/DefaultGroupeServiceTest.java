5c52e9a1e37038ef5a08fc4b20fbc32d4391d2a8
Benard Bastien <bastien.benard@cgi.com>
2022-04-22 14:52:59 +0200

    fix(neo): #MVS-56 add security to neo queries in DefaultGroupeService

---
+++ src/test/java/fr/openent/viescolaire/service/impl/DefaultGroupeServiceTest.java
@@ -0,0 +1,58 @@
+package fr.openent.viescolaire.service.impl;
+
+import io.vertx.core.Handler;
+import io.vertx.core.Vertx;
+import io.vertx.core.json.JsonObject;
+import io.vertx.ext.unit.TestContext;
+import io.vertx.ext.unit.junit.VertxUnitRunner;
+import org.entcore.common.neo4j.Neo4j;
+import org.entcore.common.neo4j.Neo4jRest;
+import org.junit.Before;
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.mockito.Mockito;
+import org.mockito.internal.util.reflection.FieldSetter;
+import org.mockito.stubbing.Answer;
+
+import java.util.Arrays;
+import java.util.List;
+
+import static org.mockito.Mockito.mock;
+
+@RunWith(VertxUnitRunner.class)
+public class DefaultGroupeServiceTest {
+    private final Neo4j neo4j = Neo4j.getInstance();
+    private final Neo4jRest neo4jRest = mock(Neo4jRest.class);
+    private Vertx vertx;
+    private DefaultGroupeService defaultGroupeService;
+
+    @Before
+    public void setUp() throws NoSuchFieldException {
+        vertx = Vertx.vertx();
+        FieldSetter.setField(neo4j, neo4j.getClass().getDeclaredField("database"), neo4jRest);
+        this.defaultGroupeService = new DefaultGroupeService();
+    }
+
+    @Test
+    public void testSearch(TestContext ctx) {
+        List<String> fields = Arrays.asList("name", "id", "student");
+
+        String expectedQuery = "MATCH (u:User {id:{userId}})-[:IN]->(:ProfileGroup)-[:DEPENDS]->(g: Class)-[:BELONGS]->(s:Structure" +
+                " {id:{structureId}}) WHERE toLower(g.{field0}) CONTAINS {query} OR toLower(g.{field1}) CONTAINS {query} OR" +
+                " toLower(g.{field2}) CONTAINS {query} RETURN g.id as id, g.name as name ORDER BY g.name UNION MATCH (u:User" +
+                " {profiles:['Student']})--(:ProfileGroup)--(c:Class)--(:ProfileGroup)--(t:User {id:{userId}}) WITH u, c" +
+                " MATCH (u)--(g)-[:DEPENDS]->(s:Structure {id:{structureId}}) WHERE (g:FunctionalGroup) AND toLower(g.{field0})" +
+                " CONTAINS {query} OR toLower(g.{field1}) CONTAINS {query} OR toLower(g.{field2}) CONTAINS {query} RETURN DISTINCT" +
+                " g.id as id, g.name as name ORDER BY g.name";
+        String expectedResult = "{\"structureId\":\"structureId\",\"userId\":\"userId\",\"query\":\"query\",\"fields0\":\"name\",\"fields1\":\"id\",\"fields2\":\"student\"}";
+        Mockito.doAnswer((Answer<Void>) invocation -> {
+            String queryResult = invocation.getArgument(0);
+            JsonObject paramsResult = invocation.getArgument(1);
+            ctx.assertEquals(queryResult, expectedQuery);
+            ctx.assertEquals(paramsResult.toString(), expectedResult);
+            return null;
+        }).when(neo4jRest).execute(Mockito.anyString(), Mockito.any(JsonObject.class), Mockito.any(Handler.class));
+
+        this.defaultGroupeService.search("structureId", "userId", "query", fields, null);
+    }
+}

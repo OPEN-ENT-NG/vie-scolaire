8a0d49275567cdb8c8658099942568be4a35fb6f
aymericdevriesere <aymeric.devriesere@cgi.com>
2022-08-05 16:21:30 +0200

    wip

--- src/main/java/fr/openent/viescolaire/controller/GroupingController.java
+++ src/main/java/fr/openent/viescolaire/controller/GroupingController.java
@@ -1,9 +1,10 @@
 package fr.openent.viescolaire.controller;
 
-import fr.openent.viescolaire.service.UserService;
+import fr.openent.viescolaire.core.constants.Field;
 import fr.openent.viescolaire.service.impl.DefaultGroupingService;
 import fr.wseduc.rs.ApiDoc;
-import fr.wseduc.rs.Get;
+import fr.wseduc.rs.Post;
+import fr.wseduc.rs.Put;
 import fr.wseduc.security.ActionType;
 import fr.wseduc.security.SecuredAction;
 import io.vertx.core.http.HttpServerRequest;
@@ -13,21 +14,41 @@ import io.vertx.core.logging.LoggerFactory;
 import org.entcore.common.controller.ControllerHelper;
 
 public class GroupingController extends ControllerHelper {
-    private DefaultGroupingService groupingService = new DefaultGroupingService();
+    private final DefaultGroupingService groupingService = new DefaultGroupingService();
     protected static final Logger log = LoggerFactory.getLogger(UserController.class);
 
-    @Get("/structures/:structureId/trombinoscope/setting")
+    @Post("/grouping/structure/:id")
     @SecuredAction(value = "", type = ActionType.AUTHENTICATED)
-    @ApiDoc("Determine structure trombinoscope if active or disable")
-    public void getTrombinoscopeSetting(HttpServerRequest request) {
-        String structureId = request.getParam("structureId");
-        groupingService.createGrouping("test", "test");
-        trombinoscopeService.getSetting(structureId, settingAsync -> {
-            if (settingAsync.failed()) {
-                renderError(request, new JsonObject().put("error", settingAsync.cause().getMessage()));
-            } else {
-                renderJson(request, new JsonObject().put("active",  settingAsync.result()));
-            }
-        });
+    @ApiDoc("Create a grouping")
+    public void createGrouping(HttpServerRequest request) {
+        String structureId = request.getParam(Field.ID);
+        String groupingName = request.getParam(Field.NAME);
+        groupingService.createGrouping(groupingName, structureId)
+                .onSuccess(res -> renderJson(request, res))
+                .onFailure(err -> renderError(request, new JsonObject().put(Field.ERROR, err.getMessage())));
     }
+
+    @Put("/grouping/:id")
+    @SecuredAction(value = "", type = ActionType.AUTHENTICATED)
+    @ApiDoc("Update a grouping")
+    public void updateGrouping(HttpServerRequest request) {
+        String groupingId = request.getParam(Field.ID);
+        String groupingName = request.getParam(Field.NAME);
+        groupingService.updateGrouping(groupingId, groupingName)
+                .onSuccess(res -> renderJson(request, res))
+                .onFailure(err -> renderError(request, new JsonObject().put(Field.ERROR, err.getMessage())));
+    }
+
+    @Put("/grouping/:grouping_id/add")
+    @SecuredAction(value = "", type = ActionType.AUTHENTICATED)
+    @ApiDoc("Add classes or groups to a grouping")
+    public void addGrouping(HttpServerRequest request) {
+        String groupingId = request.getParam(Field.GROUPING_ID);
+        String groupId = request.getParam(Field.GROUP_ID);
+        String classId = request.getParam(Field.CLASS_ID);
+        groupingService.addToGrouping(groupingId, groupId, classId)
+                .onSuccess(res -> renderJson(request, res))
+                .onFailure(err -> renderError(request, new JsonObject().put(Field.ERROR, err.getMessage())));
+    }
+
 }
--- src/main/java/fr/openent/viescolaire/service/impl/DefaultGroupingService.java
+++ src/main/java/fr/openent/viescolaire/service/impl/DefaultGroupingService.java
@@ -1,19 +1,30 @@
 package fr.openent.viescolaire.service.impl;
 
+import fr.openent.viescolaire.helper.PromiseHelper;
 import fr.openent.viescolaire.service.GroupingService;
 import fr.openent.viescolaire.utils.DateHelper;
 import io.vertx.core.Future;
 import io.vertx.core.Promise;
 import io.vertx.core.json.JsonArray;
 import io.vertx.core.json.JsonObject;
+import io.vertx.core.logging.Logger;
+import io.vertx.core.logging.LoggerFactory;
 import org.entcore.common.sql.Sql;
 import org.entcore.common.sql.SqlResult;
 
 import java.util.UUID;
 
 public class DefaultGroupingService implements GroupingService {
+    protected static final Logger log = LoggerFactory.getLogger(DefaultGroupingService.class);
+
+    /**
+     * Create a new grouping
+     * @param name          Name of the new grouping
+     * @param structureId   Identifier of the structure to which the grouping belongs
+     * @return              Promise with the status of the grouping creation.
+     */
     public Future<JsonObject> createGrouping(String name, String structureId) {
-        Promise promise = Promise.promise();
+        Promise<JsonObject> promise = Promise.promise();
         String uuid = UUID.randomUUID().toString();
         JsonArray values = new JsonArray();
         StringBuilder query = new StringBuilder();
@@ -28,9 +39,70 @@ public class DefaultGroupingService implements GroupingService {
             if(res.isRight())
                 promise.complete(res.right().getValue());
             else {
-                promise.fail("error.while.creating.grouîng");
+                String messageToFormat = "[vie-scolaire@%s::createGrouping] Error while creating grouping : %s";
+                PromiseHelper.reject(log, messageToFormat, this.getClass().getSimpleName(), new Exception(res.left().getValue()), promise);
+
             }
         }));
     return promise.future();
     }
+
+    /**
+     * Update the grouping
+     * @param groupingId    Identifier of the grouping
+     * @param name          New grouping name
+     * @return              Promise with the status of the update
+     */
+    @Override
+    public Future<JsonObject> updateGrouping(String groupingId ,String name) {
+        Promise<JsonObject> promise = Promise.promise();
+        JsonArray values = new JsonArray();
+        StringBuilder query = new StringBuilder();
+        query.append("UPDATE grouping")
+                .append("SET name = ? WHERE id = ?");
+        values.add(name);
+        values.add(groupingId);
+        Sql.getInstance().prepared(query.toString(), values, SqlResult.validUniqueResultHandler(res -> {
+            if (res.isRight())
+                promise.complete(res.right().getValue());
+            else {
+                String messageToFormat = "[vie-scolaire@%s::updateGrouping] Error while updating grouping : %s";
+                PromiseHelper.reject(log, messageToFormat, this.getClass().getSimpleName(), new Exception(res.left().getValue()), promise);
+            }
+        }));
+        return promise.future();
+    }
+
+    /**
+     * Add classes and groups to the grouping
+     * @param groupingId    Identifier of the grouping
+     * @param groupId       Identifier of the group
+     * @param classId       Identifier of the class
+     * @return              Promise with the status of the operation.
+     */
+    @Override
+    public Future<JsonObject> addToGrouping(String groupingId, String groupId, String classId) {
+        Promise<JsonObject> promise = Promise.promise();
+        JsonArray values = new JsonArray();
+        StringBuilder query = new StringBuilder();
+        query.append("INSERT INTO rel_grouping_class(grouping_id, class_id, group_id, created_at, updated_at) ")
+                .append("VALUES(?, ?, ?, ?, ?)");
+        values.add(groupingId);
+        values.add(classId);
+        values.add(groupId);
+        values.add(DateHelper.getCurrentDate(DateHelper.HOUR_MINUTES_SECONDS));
+        values.add(DateHelper.getCurrentDate(DateHelper.HOUR_MINUTES_SECONDS));
+        Sql.getInstance().prepared(query.toString(), values, SqlResult.validUniqueResultHandler(res -> {
+            if(res.isRight())
+                promise.complete(res.right().getValue());
+            else {
+                String messageToFormat = "[vie-scolaire@%s::addGrouping] Error while adding classes or groups to grouping : %s";
+                PromiseHelper.reject(log, messageToFormat, this.getClass().getSimpleName(), new Exception(res.left().getValue()), promise);
+
+            }
+        }));
+        return promise.future();
+    }
+
+
 }

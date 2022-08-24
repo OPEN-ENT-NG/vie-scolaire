375ed3ed6faed162a2f74d12284c8e427d79318e
BastienBenard <95362455+BastienBenard@users.noreply.github.com>
2022-08-24 09:25:51 +0200

    fix(security): #MVS-74 fix vulnerability TrombinoscopeController (#46)
    Co-authored-by: Benard Bastien <bastien.benard@cgi.com>

--- src/main/java/fr/openent/viescolaire/security/Trombinoscope/ManageTrombinoscope.java
+++ src/main/java/fr/openent/viescolaire/security/Trombinoscope/ManageTrombinoscope.java
@@ -1,5 +1,6 @@
 package fr.openent.viescolaire.security.Trombinoscope;
 
+import fr.openent.viescolaire.core.constants.Field;
 import fr.openent.viescolaire.security.WorkflowActionUtils;
 import fr.openent.viescolaire.security.WorkflowActions;
 import fr.wseduc.webutils.http.Binding;
@@ -11,6 +12,7 @@ import org.entcore.common.user.UserInfos;
 public class ManageTrombinoscope implements ResourcesProvider {
     @Override
     public void authorize(HttpServerRequest request, Binding binding, UserInfos user, Handler<Boolean> handler) {
-        handler.handle(WorkflowActionUtils.hasRight(user, WorkflowActions.MANAGE_TROMBINOSCOPE.toString()));
+        String structure = request.getParam(Field.STRUCTUREID);
+        handler.handle(user.getStructures().contains(structure) && WorkflowActionUtils.hasRight(user, WorkflowActions.MANAGE_TROMBINOSCOPE.toString()));
     }
 }

08e399b40abb22c397b57310e1334ae526622878
simon.chaval <69917847+simonchaval@users.noreply.github.com>
2023-03-08 15:25:23 +0100

    fix(security): #MVS-102 fixing security issues in MultiteachingController (#85)
    * fix(security): #MVS-102 fixing security issues in MultiteachingController
    
    * fix(security): #MVS-102 fixing error in test
    
    * fix(security): #MVS-102 modify unnecessary condition

--- src/main/java/fr/openent/viescolaire/security/StructureRight.java
+++ src/main/java/fr/openent/viescolaire/security/StructureRight.java
@@ -1,6 +1,5 @@
 package fr.openent.viescolaire.security;
 
-import fr.openent.viescolaire.core.constants.*;
 import fr.wseduc.webutils.http.*;
 import io.vertx.core.*;
 import io.vertx.core.http.*;
@@ -10,7 +9,7 @@ import org.entcore.common.user.*;
 public class StructureRight implements ResourcesProvider {
     @Override
     public void authorize(HttpServerRequest request, Binding binding, UserInfos user, Handler<Boolean> handler) {
-        String structure = request.getParam(Field.STRUCTUREID);
-        handler.handle(user.getStructures().contains(structure));
+        String structureId = WorkflowActionUtils.getParamStructure(request);
+        handler.handle(user.getStructures().contains(structureId));
     }
 }

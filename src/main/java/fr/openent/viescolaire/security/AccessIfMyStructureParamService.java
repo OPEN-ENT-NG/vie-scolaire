55628cae97a7fcd387bd3839bfac751d24f254a5
simon.chaval <simon.chaval@cgi.com>
2023-03-08 14:53:38 +0100

    fix(security): #MVS-102 modify unnecessary condition

--- src/main/java/fr/openent/viescolaire/security/AccessIfMyStructureParamService.java
+++ src/main/java/fr/openent/viescolaire/security/AccessIfMyStructureParamService.java
@@ -10,10 +10,6 @@ public class AccessIfMyStructureParamService implements ResourcesProvider {
     @Override
     public void authorize(final HttpServerRequest request, Binding binding, UserInfos user, final Handler<Boolean> handler) {
         String structureId = WorkflowActionUtils.getParamStructure(request);
-        if (structureId == null) {
-            handler.handle(false);
-        } else {
-            handler.handle(user.getStructures().contains(structureId) && WorkflowActionUtils.hasRight(user, WorkflowActionUtils.PARAM_SERVICES_RIGHT));
-        }
+        handler.handle(structureId != null && user.getStructures().contains(structureId) && WorkflowActionUtils.hasRight(user, WorkflowActionUtils.PARAM_SERVICES_RIGHT));
     }
 }

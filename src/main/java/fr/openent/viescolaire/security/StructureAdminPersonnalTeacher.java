7a6f0533b8086582860850e2d1a8bbd2708f7905
simon.chaval <69917847+simonchaval@users.noreply.github.com>
2023-03-15 09:59:16 +0100

    fix(security): #MVS-103 added security on api in ClasseController (#89)
    * fix(security): #MVS-103 added security on api in ClasseController
    
    * fix(security): #MVS-103 modify StructureAdminPersonnalTeacher
    
    * fix(security): #MVS-103 remove unused imports in ClasseController
    
    ---------
    
    Co-authored-by: BOES <gregoryboes33@gmail.com>

---
+++ src/main/java/fr/openent/viescolaire/security/StructureAdminPersonnalTeacher.java
@@ -0,0 +1,16 @@
+package fr.openent.viescolaire.security;
+
+import fr.wseduc.webutils.http.Binding;
+import io.vertx.core.Handler;
+import io.vertx.core.http.HttpServerRequest;
+import org.entcore.common.http.filter.ResourcesProvider;
+import org.entcore.common.user.UserInfos;
+
+public class StructureAdminPersonnalTeacher implements ResourcesProvider {
+    @Override
+    public void authorize(final HttpServerRequest request, Binding binding, final UserInfos user, final Handler<Boolean> handler) {
+        String structureId = WorkflowActionUtils.getParamStructure(request);
+        handler.handle(structureId != null && user.getStructures().contains(structureId) && (WorkflowActionUtils.hasRight(user, WorkflowActionUtils.ADMIN_RIGHT) ||
+                "Personnel".equals(user.getType()) || "Teacher".equals(user.getType())));
+    }
+}

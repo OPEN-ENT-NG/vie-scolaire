76c82c8382ab1b7e04bf750786cf67b1f249ce09
Jinhda Douang <Jinhda.douangphrachanh@cgi.com>
2023-03-30 08:59:24 +0200

    feat(memento): #MVS-110 add check access memento (#103)
    feat(memento): #MVS-110 add check access memento
    
    Co-authored-by: jinhda douang <s>

--- src/main/resources/public/modules/viescolaire/services/MementoService.ts
+++ src/main/resources/public/modules/viescolaire/services/MementoService.ts
@@ -1,10 +1,12 @@
 import {ng} from 'entcore';
 import http, {AxiosResponse} from 'axios';
+import {MementoAccess} from "../models/memento.model";
 
 export interface IMementoService {
     saveComment(studentId: string, comment: string): Promise<AxiosResponse>;
 
     updateRelativePriorities(studentId: string, relativeIds: Array<string>): Promise<AxiosResponse>;
+    checkAccess(): Promise<MementoAccess>;
 }
 
 export const mementoService: IMementoService = {
@@ -15,7 +17,11 @@ export const mementoService: IMementoService = {
 
     async updateRelativePriorities(studentId: string, relativeIds: Array<string>): Promise<AxiosResponse> {
         return http.put(`/viescolaire/memento/students/${studentId}/relatives/priority`, {'relativeIds': relativeIds});
-    }
+    },
+
+    async checkAccess(): Promise<MementoAccess> {
+        return http.get(`/viescolaire/memento/access`).then((res: AxiosResponse) => res.data);
+    },
 };
 
 export const MementoService = ng.service('MementoService', (): IMementoService => mementoService);

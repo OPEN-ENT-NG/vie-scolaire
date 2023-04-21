17f83dfaba20263097277394a0d6e8d54812e716
Flavien Martineau <45894263+flmartineau@users.noreply.github.com>
2023-04-21 17:45:25 +0200

    feat(init-1D): #MA-1066 add init schoolyear period (#109)

---
+++ src/main/java/fr/openent/viescolaire/core/constants/PeriodeCode.java
@@ -0,0 +1,6 @@
+package fr.openent.viescolaire.core.constants;
+
+public class PeriodeCode {
+    public static final String YEAR = "YEAR";
+    public static final String EXCLUSION = "EXCLUSION";
+}

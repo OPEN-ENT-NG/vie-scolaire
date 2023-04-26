f1548d2717d34f3ce62f6b5bf845e6ab8fd168b1
alex.delaveau <alex.delaveau@cgi.com>
2023-04-26 12:18:44 +0200

    fix(security): MA-1107 fix module activation

--- src/main/java/fr/openent/viescolaire/security/AdministratorRight.java
+++ src/main/java/fr/openent/viescolaire/security/AdministratorRight.java
@@ -14,7 +14,6 @@ public class AdministratorRight implements ResourcesProvider {
     public void authorize(HttpServerRequest resourceRequest, Binding binding, UserInfos user, Handler<Boolean> handler) {
         String structureId = WorkflowActionUtils.getParamStructure(resourceRequest);
         boolean allowViesco = WorkflowActionUtils.hasRight(user, WorkflowActions.ADMIN_RIGHT.toString());
-        boolean allowCompetences = WorkflowActionUtils.hasRight(user, WorkflowActions.COMPETENCES_ACCESS.toString());
-        handler.handle( structureId != null && user.getStructures().contains(structureId) && allowViesco && allowCompetences);
+        handler.handle( structureId != null && user.getStructures().contains(structureId) && allowViesco);
     }
 }

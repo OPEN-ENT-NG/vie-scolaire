5317ee3d34d32826b220a95818c5e64a4e13454c
Flavien Martineau <45894263+flmartineau@users.noreply.github.com>
2023-05-10 15:32:23 +0200

    feat(init-1D): #MVS-126 add reset API for initialization (#115)

--- src/main/java/fr/openent/viescolaire/service/impl/DefaultInitService.java
+++ src/main/java/fr/openent/viescolaire/service/impl/DefaultInitService.java
@@ -30,21 +30,24 @@ public class DefaultInitService implements InitService {
     private final Sql sql;
     private final MongoDb mongoDb;
     private final EventBus eb;
+    private final JsonObject config;
     private static final String SLOTPROFILE_COLLECTION = "slotprofile";
 
     private final TimeSlotService timeSlotService;
     private final UserService userService;
-
     private final ServicesService servicesService;
+    private final MatiereService matiereService;
     private static final Logger LOGGER = LoggerFactory.getLogger(DefaultInitService.class);
     public DefaultInitService(ServiceFactory serviceFactory) {
         this.neo4j = serviceFactory.neo4j();
         this.sql = serviceFactory.sql();
         this.mongoDb = serviceFactory.mongoDb();
         this.eb = serviceFactory.getEventbus();
+        this.config = serviceFactory.config();
         this.timeSlotService = serviceFactory.timeSlotService();
         this.userService = serviceFactory.userService();
         this.servicesService = serviceFactory.servicesService();
+        this.matiereService = serviceFactory.matiereService();
     }
 
     @Override
@@ -114,15 +117,34 @@ public class DefaultInitService implements InitService {
     }
 
     @Override
+    public Future<Void> setInitializationStatus(String structureId, boolean status) {
+        Promise<Void> promise = Promise.promise();
+
+        String query = "UPDATE " + VSCO_SCHEMA + ".settings SET initialized = ? WHERE structure_id = ?";
+        JsonArray params = new JsonArray().add(status).add(structureId);
+
+        sql.prepared(query, params, SqlResult.validUniqueResultHandler(res -> {
+            if (res.isRight()) {
+                promise.complete();
+            } else {
+                LOGGER.error(String.format("[Viescolaire@%s::setInitializationStatus] Failed to set initialization status : %s",
+                        this.getClass().getSimpleName(), res.left().getValue()), res.left().getValue());
+                promise.fail(res.left().getValue());
+            }
+        }));
+        return promise.future();
+    }
+
+    @Override
     public Future<SlotProfile> initTimeSlots(String structureId, String structureName, User owner, InitFormTimetable timetable,
-                                      String locale, String acceptLanguage) {
+                                             String locale, String acceptLanguage) {
         Promise<SlotProfile> promise = Promise.promise();
 
         SlotProfile slotProfile = new SlotProfile();
 
         slotProfile.setId(UUID.randomUUID().toString());
         slotProfile.setName(String.format("%s %s", I18n.getInstance().translate("viescolaire.time.grid",
-                        locale, acceptLanguage), structureName));
+                locale, acceptLanguage), structureName));
         slotProfile.setSchoolId(structureId);
         slotProfile.setSlots(timetable.getSlots(
                 I18n.getInstance().translate("viescolaire.time.grid.morning.prefix", locale, acceptLanguage),
@@ -132,6 +154,40 @@ public class DefaultInitService implements InitService {
         slotProfile.setModified(MongoDb.now());
         slotProfile.setOwner(owner);
 
+        this.getSlotProfile(structureId, slotProfile)
+                .compose(res -> {
+                    if (res != null) {
+                        return Future.succeededFuture(res);
+                    } else {
+                        return this.createSlotProfile(structureId, slotProfile, timetable);
+                    }
+                });
+
+        return promise.future();
+    }
+
+    private Future<SlotProfile> getSlotProfile(String structureId, SlotProfile slotProfile) {
+        Promise<SlotProfile> promise = Promise.promise();
+        mongoDb.findOne(SLOTPROFILE_COLLECTION, new JsonObject()
+                        .put(Field.NAME, slotProfile.getName())
+                        .put(Field.SCHOOLID, structureId),
+                MongoDbResult.validResultHandler(res -> {
+                    if (res.isRight() && res.right().getValue() != null &&
+                            !res.right().getValue().isEmpty() && new SlotProfile(res.right().getValue()).isEquals(slotProfile)) {
+                        promise.complete(new SlotProfile(res.right().getValue()));
+                    } else if (res.isLeft()){
+                        String message = String.format("[Viescolaire@%s::initTimeSlots] Failed to retrieve slot profile : %s",
+                                this.getClass().getSimpleName(), res.left().getValue());
+                        LOGGER.error(message);
+                        promise.fail(res.left().getValue());
+                    }
+                }));
+
+        return promise.future();
+    }
+
+    private Future<SlotProfile> createSlotProfile(String structureId, SlotProfile slotProfile, InitFormTimetable timetable) {
+        Promise<SlotProfile> promise = Promise.promise();
         mongoDb.insert(SLOTPROFILE_COLLECTION, slotProfile.toJson(), MongoDbResult.validResultHandler(results -> {
             if (results.isLeft()) {
                 String message = String.format("[Viescolaire@%s::initTimeSlots] Failed to create slot profile",
@@ -140,37 +196,54 @@ public class DefaultInitService implements InitService {
                 promise.fail(results.left().getValue());
             } else {
                 this.timeSlotService.saveTimeProfil(
-                        new JsonObject()
-                                .put(Field.ID, slotProfile.getId())
-                                .put(Field.ID_STRUCTURE, structureId))
+                                new JsonObject()
+                                        .put(Field.ID, slotProfile.getId())
+                                        .put(Field.ID_STRUCTURE, structureId))
                         .compose(v -> this.timeSlotService.updateEndOfHalfDay(slotProfile.getId(),
                                 timetable.getMorning().getString(Field.ENDHOUR), structureId))
                         .onFailure(fail -> {
-                            LOGGER.error(String.format("[Viescolaire@%s::initTimeSlots] Failed to save and update end of half day",
-                                    this.getClass().getSimpleName()), fail);
+                            LOGGER.error(String.format("[Viescolaire@%s::createSlotProfile] Failed to save and update " +
+                                            "end of half day : %s",
+                                    this.getClass().getSimpleName(), fail.getMessage()), fail.getMessage());
                             promise.fail(fail);
                         })
                         .onSuccess(success -> promise.complete(slotProfile));
             }
         }));
-
         return promise.future();
     }
 
     @Override
-    public Future<JsonObject> initSubjects(String structureId, String label, String code) {
-        Promise<JsonObject> promise = Promise.promise();
+    public Future<SubjectModel> initSubject(String structureId, SubjectModel subject) {
+        Promise<SubjectModel> promise = Promise.promise();
 
-        JsonObject subject = new JsonObject()
+        JsonObject subjectParams = new JsonObject()
                 .put(Field.STRUCTUREID, structureId)
-                .put(Field.LABEL, label)
-                .put(Field.CODE, code);
+                .put(Field.LABEL, subject.getLabel())
+                .put(Field.CODE, subject.getCode());
 
-        JsonObject action = new JsonObject()
-                .put(Field.ACTION, "manual-add-subject")
-                .put(Field.SUBJECT, subject);
+        this.matiereService.getSubjectByCode(structureId, subject.getCode())
+                .onFailure(promise::fail)
+                .onSuccess(sub -> {
+                    boolean isSubjectExist = sub != null;
+                    JsonObject action = new JsonObject()
+                            .put(Field.ACTION, isSubjectExist ? "manual-update-subject" : "manual-add-subject")
+                            .put(Field.SUBJECT, isSubjectExist ? subjectParams.put(Field.ID, sub.getId()) : subjectParams);
 
-        eb.request(FEEDER_ADDRESS, action, MessageResponseHandler.messageJsonObjectHandler(FutureHelper.handlerEitherPromise(promise)));
+                    eb.request(FEEDER_ADDRESS, action, MessageResponseHandler.messageJsonObjectHandler(res -> {
+                        JsonArray resArray = res.isRight() ? res.right().getValue()
+                                .getJsonArray(Field.RESULTS, new JsonArray()) : new JsonArray();
+
+                        if (res.isLeft() || resArray.isEmpty() || resArray.getJsonArray(0).isEmpty() ||
+                                resArray.getJsonArray(0).getJsonObject(0).isEmpty()) {
+                            LOGGER.error(String.format("[Viescolaire@%s::initSubjects] Failed to create subject",
+                                    this.getClass().getSimpleName()), res.isLeft() ? res.left().getValue() : resArray);
+                            promise.fail(res.left().getValue());
+                        } else {
+                            promise.complete(new SubjectModel(resArray.getJsonArray(0).getJsonObject(0)));
+                        }
+                    }));
+                });
 
         return promise.future();
     }
@@ -275,6 +348,38 @@ public class DefaultInitService implements InitService {
 
         eb.request(EDT_ADDRESS, action, MessageResponseHandler.messageJsonObjectHandler(FutureHelper.handlerEitherPromise(promise)));
 
+
+        return promise.future();
+    }
+
+    @Override
+    public Future<Void> resetInit(String structureId) {
+        Promise<Void> promise = Promise.promise();
+        this.matiereService.getSubjectByCode(structureId, "999999")
+                .onFailure(promise::fail)
+                .onSuccess(subject -> this.servicesService.deleteServiceBySubjectId(structureId, subject.getId())
+                        .compose(res -> this.resetCourses(structureId, subject.getId()))
+                        .compose(res -> this.setInitializationStatus(structureId, false))
+                        .onSuccess(res -> promise.complete())
+                        .onFailure(fail -> {
+                            LOGGER.error(String.format("[Viescolaire@%s::resetInit] Failed to reset init : %s",
+                                    this.getClass().getSimpleName(), fail.getMessage()), fail);
+                            promise.fail(fail);
+                        }));
+
+        return promise.future();
+    }
+
+
+    private Future<JsonObject> resetCourses(String structureId, String subjectId) {
+        Promise<JsonObject> promise = Promise.promise();
+
+        JsonObject action = new JsonObject()
+                .put(Field.ACTION, "delete-courses-subject")
+                .put(Field.STRUCTUREID, structureId)
+                .put(Field.SUBJECTID, subjectId);
+
+        eb.request(EDT_ADDRESS, action, MessageResponseHandler.messageJsonObjectHandler(FutureHelper.handlerEitherPromise(promise)));
 
         return promise.future();
     }
